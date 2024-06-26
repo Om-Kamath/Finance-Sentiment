1758b76c3b8cbab517892156c4a772132610b558
Om Kamath <omganesh402@gmail.com>
2024-06-26 17:28:32 +0530

    Added key insights

--- main.py
+++ main.py
@@ -5,10 +5,18 @@ import feedparser
 import streamviz
 import requests
 
-API_URL = "https://flowise.codesm.xyz/api/v1/prediction/2702a14a-10b7-4bba-b7a4-aad5ddef2647"
+API_URL_SENTIMENT = "https://flowise.codesm.xyz/api/v1/prediction/2702a14a-10b7-4bba-b7a4-aad5ddef2647"
+API_URL_INSIGHTS = "https://flowise.codesm.xyz/api/v1/prediction/5923f507-b119-401a-9e0c-65a53e6666bf"
 headers = {"Authorization": f"Bearer {st.secrets['FLOWISE']}"}
 
+
 df = pd.read_csv('us_symbols.csv')
+
+st.markdown("""
+            <style>
+            .stPlotlyChart {
+                height:275px !important;
+            """, unsafe_allow_html=True)
 
 sentiment_scores = {
     "Very Positive": 2,
@@ -17,6 +25,21 @@ sentiment_scores = {
     "Negative": -1,
     "Very Negative": -2
 }
+
+def get_key_insights(news_summary, final_score):
+    # fetch only summary and sentiment
+    insights = []
+    for news in news_summary:
+        insights.append({
+            'summary': news['summary'],
+            'sentiment': news['sentiment']
+        })
+    payload = {
+        'question': f"Final Score:{final_score},{insights}",
+    }
+    response = requests.post(API_URL_INSIGHTS, json=payload, headers=headers)
+    key_insights = response.json()['text']
+    return key_insights
 
 def get_feed_summary(ticker, stock):
     url = f'https://feeds.finance.yahoo.com/rss/2.0/headline?s={ticker}&region=US&lang=en-US'
@@ -27,7 +50,7 @@ def get_feed_summary(ticker, stock):
             payload = {
                 'question': f"Stock:{stock}, Title:{news['title']}, Summary:{news.get('summary', 'No summary available')}",
             }
-            response = requests.post(API_URL, json=payload, headers=headers)
+            response = requests.post(API_URL_SENTIMENT, json=payload, headers=headers)
             sentiment = response.json()['text']
             print(sentiment)
             filtered_news = {
@@ -85,14 +108,16 @@ def main():
             streamviz.gauge(
                 final_score, 
                 gTitle="Market Mood Index", 
-                gMode="gauge", 
+                gMode="number+gauge", 
                 gSize="MED", 
                 sFix=None,
                 gcLow="#FF1708", 
                 gcMid="#FF9400", 
                 gcHigh="#1B8720"
             )
-            
+            key_insights = get_key_insights(news_summary, final_score).replace('$','\$')
+            st.markdown(f"### Key Insights")
+            st.markdown(key_insights)
             st.markdown(f"### Articles related to {stock}")
             # Then display each news summary
             for news in news_summary:
@@ -101,8 +126,8 @@ def main():
                 st.write(news['summary'].replace('$','\$'))
                 sentiment = news.get('sentiment', 'Neutral')  # Default to 'Neutral' if not present
                 if sentiment in sentiment_scores:
+                    print(sentiment)
                     score = sentiment_scores[sentiment]
-                    # Determine the color based on sentiment
                     color = "green" if score > 0 else "red" if score < 0 else "orange"
                     st.markdown(f":{color}-background[{sentiment}]")
                 else:
